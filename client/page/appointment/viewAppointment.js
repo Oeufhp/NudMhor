bde48aafcf3005b99015dd5f92778261a6265fd7
Pitarn Harnpanichpun <pitarnh@outlook.com>
2016-10-24 22:15:32 +0700

    menu: add modal to view appointment

---
+++ client/page/appointment/viewAppointment.js
@@ -0,0 +1,13 @@
+Template.body.events({
+    'submit #viewAppointmentForm': function(event){
+        event.preventDefault();
+        $('#viewAppointment1Modal').modal('toggle');
+        $('#viewAppointment2Modal').modal({backdrop: 'static', keyboard: false});
+    }
+});
+Template.body.rendered=function(){
+     $('#datepicker-appt').datepicker({
+            format: "dd/mm/yyyy",
+            maxViewMode: 2,
+        });
+}
