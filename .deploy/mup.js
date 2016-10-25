797b93f6a3b74e023c3e4687c551bfe8e2840295
snn2spade <lostunevol@gmail.com>
2016-10-26 03:57:32 +0700

    ตบตีกับ Automate deploy WTF

---
+++ .deploy/mup.js
@@ -0,0 +1,41 @@
+module.exports = {
+  servers: {
+    one: {
+      host: '128.199.96.46',
+      username: 'root',
+      // pem:
+      password: 'shadowfiend38'
+      // or leave blank for authenticate from ssh-agent
+    }
+  },
+
+  meteor: {
+    name: 'nudmhor',
+    path: '.',
+    servers: {
+      one: {}
+    },
+    buildOptions: {
+      serverOnly: true,
+    },
+    docker: {
+      //image: 'kadirahq/meteord', // (optional)
+      image: 'abernix/meteord:base' // use this image if using Meteor 1.4+
+    },
+    env: {
+      //ROOT_URL: '128.199.96.46',
+      MONGO_URL: 'mongodb://localhost/meteor'
+    },
+
+    //dockerImage: 'kadirahq/meteord'
+    deployCheckWaitTime: 60
+  },
+
+  mongo: {
+    oplog: true,
+    port: 27017,
+    servers: {
+      one: {},
+    },
+  },
+};
