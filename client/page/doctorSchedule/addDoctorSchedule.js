82f6008f4580da696a19e12a4a84df0cea71222d
snn2spade <lostunevol@gmail.com>
2016-10-30 13:55:12 +0700

    #1 Fixed bug new appointment

--- client/page/doctorSchedule/addDoctorSchedule.js
+++ client/page/doctorSchedule/addDoctorSchedule.js
@@ -4,28 +4,7 @@ Template.body.events({
       console.log('adddoctorscheduleForm is submited');
       $('#adddoctorscheduleModal').modal('toggle');
       $('#adddoctorscheduleModal2').modal({backdrop: 'static', keyboard: false});
-    },
-    'click #newdatetimeButton': function(event){
-      console.log("click newdatetimeButton");
-      $('#newdatetimeOption').toggle();
-    },
-    'change #newdatetimeOption':function(event){
-      console.log('newdatetimeOption value has changed');
-      $('#newdatetimeLabel').text($('#newdatetimeOption').val());
-    },
-    'submit #makeappointmentForm2': function(event){
-      event.preventDefault();
-      console.log('makeappointmentForm2 is submited');
-      $('#makeappointmentModal2').modal('toggle');
-      $('#choosenDoctorLabel').text();
-      $('#makeappointmentModal3').modal({backdrop: 'static', keyboard: false});
-    },
-    'submit #makeappointmentForm': function(event){
-      event.preventDefault();
-      console.log('makeappointmentForm is submited');
-      $('#makeappointmentModal').modal('toggle');
-      $('#makeappointmentModal2').modal({backdrop: 'static', keyboard: false});
-    },
+    }
 });
 
 Template.body.rendered=function(){
