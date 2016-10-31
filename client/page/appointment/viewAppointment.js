96935b67192f28ac885b1dd8a8d11a18eaaa49a6
Pitarn Harnpanichpun <pitarnh@outlook.com>
2016-10-31 19:03:59 +0700

    receptionist-page: reroute

--- client/page/appointment/viewAppointment.js
+++ client/page/appointment/viewAppointment.js
@@ -1,8 +1,16 @@
 Template.body.events({
     'submit #viewAppointmentForm': function(event){
         event.preventDefault();
-        $('#viewAppointment1Modal').modal('toggle');
+        $('#viewAppointmentModal').modal('toggle');
         $('#viewAppointment2Modal').modal({backdrop: 'static', keyboard: false});
+    },
+    'click #appt-search-btn': function(event){
+        event.preventDefault();
+        Router.go('/viewAppointment/results');
+        $('#viewAppointmentModal').modal('toggle');
     }
 });
 
+Template.body.rendered=function(){
+  $('#viewAppointmentModal').modal({backdrop: 'static', keyboard: false});
+}
