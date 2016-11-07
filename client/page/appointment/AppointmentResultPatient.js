5493282ab4ee1b7a526635f60c3bfe1b4422bae1
Pitarn Harnpanichpun <pitarnh@outlook.com>
2016-11-07 10:52:04 +0700

    doctor-page: remove create appointment button

---
+++ client/page/appointment/AppointmentResultPatient.js
@@ -0,0 +1,6 @@
+Template.body.events({
+    'click #view-btn1': function(event){
+        event.preventDefault();
+        
+    }
+});
--- client/page/appointment/viewAppointment.js
+++ client/page/appointment/viewAppointment.js
@@ -8,13 +8,13 @@ Template.body.events({
         event.preventDefault();
         Router.go('/viewAppointment/results');
         $('#viewAppointmentModal').modal('toggle');
-    }   
-    //  },
-    // 'click #view-appt-div' : function(event){
-    //     event.preventDefault();
-    //     console.info("Patient View Appointment select");
-    //     Router.go('/viewAppointment/results');
-    // }
+    },   
+    'click #del-appt-btn' : function(event){
+        event.preventDefault();
+        $('#viewAppointmentModal3').modal('toggle');
+        $('#viewAppointmentModal2').modal('toggle');
+        
+    }
 });
 
 Template.body.rendered=function(){
