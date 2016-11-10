cffdec004dc1626398a8cb9a757b3853f0a45886
tunnuttun <tunnuttun@gmail.com>
2016-11-10 09:02:04 +0700

    implementing addHospitalEmp js

--- client/page/manageHospitalEmployee/addHospitalEmp.js
+++ client/page/manageHospitalEmployee/addHospitalEmp.js
@@ -1,13 +1,47 @@
-Template.body.events({
-  'submit #addEmployeeForm': function(event){
-    event.preventDefault();
-    console.log('addEmployeeForm is submited');
-    $('#addEmployeeModal').modal('hide');
-    $('#addEmployeeModal2').modal({backdrop: 'static', keyboard: false});
-  },
-});
-Template.body.rendered(function(){
-  $('#datepicker-addemployee').datepicker().on('changeDate', function(ev){
-    $(this).datepicker('hide');
+if(Meteor.isClient){
+  Template.body.rendered(function(){
+    $('#datepicker-addemployee').datepicker().on('changeDate', function(ev){
+      $(this).datepicker('hide');
+    });
   });
-})
+  Template.body.events({
+    'submit #addEmployeeForm': function(event){
+      event.preventDefault();
+      console.log('addEmployeeForm is submited');
+      let cid = event.target.registerCID.value.trim();
+      let fname = event.target.firstname.value.trim();
+			let lname = event.target.lastname.value.trim();
+      let password = event.target.registerPassword.value.trim();
+      let email = event.target.email.value.trim();
+      let tel = event.target.tel.value.trim();
+      let birthdate = event.target.birthdate.value;
+      let role = event.traget.role.value;
+      if(cid == "" || cid.length != 13) {
+        Bert.alert({title:'กรุญาใส่รหัสบัตรประชาชน 13 หลัก',type:'danger',style: 'growl-top-right'});
+        return;
+      }
+      if(password == ""||(password.length<6 || password.length>10) ){
+        Bert.alert({title:'กรุณาใส่พาสเวิร์ด 6-10 หลัก',type:'danger',style: 'growl-top-right'});
+        return;
+      }
+      if(email ==""||fname==""||lname==""||tel==""||gender==""||birthdate==""){
+        Bert.alert({title:'กรุณากรอกข้อมูลให้ครบถ้วน',type:'danger',style: 'growl-top-right'});
+        return;
+      } 
+
+      let user = {cid:cid,password:password,email:email,fname:fname,lname:lname,tel:tel,gender:gender,birthdate:birthdate,role:role};
+      Meteor.call('addemployee',user,function(err,result){
+        console.log("result : "+result);
+        if(err!=null){
+          Bert.alert({title: 'Already used citizenID',type: 'danger',style: 'growl-top-right',icon: 'fa-key'});
+        }
+        else{
+          $('#addEmployeeModal').modal('hide');
+          $('#addEmployeeModal2').modal({backdrop: 'static', keyboard: false});
+          Bert.alert({title: 'Register successful',type: 'success',style: 'growl-top-right',icon: 'fa-check'});
+        }
+      });    
+    },
+  });
+}
+
