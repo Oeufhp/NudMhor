5722494a0d136e06e808976cba0c112a2eeec4b5
tunnuttun <tunnuttun@gmail.com>
2016-11-16 22:37:24 +0700

    Complete addDoctorSchedule.

--- client/page/doctorSchedule/addDoctorSchedule.js
+++ client/page/doctorSchedule/addDoctorSchedule.js
@@ -7,19 +7,19 @@ if(Meteor.isClient){
         let eid = event.target.eid.value.trim();
         let time = event.target.time.value;
         let date = event.target.date.value;
-        Meteor.call('addDoctorSchedule',eid,date,time,function(err,user){
-          console.log('result : ' + user);
+        Meteor.call('searchDoctor',eid,function(err,name){
+          console.log('result : ' + name);
           if(err!=null){
-            Bert.alert({title: 'No doctor in system', type: 'danger',style:'growl-top-right',icon: 'fa-key'});
+            Bert.alert({title: 'รหัสพนักงานของแพทย์ที่ระบุไม่มีอยู่ในระบบ กรุณาระบุใหม่อีกครั้ง', type: 'danger',style:'growl-top-right',icon: 'fa-key'});
           }
           else{
+            Meteor.call('addDoctorSchedule',eid,date,time);
             $('#adddoctorscheduleModal').modal('toggle');
             $('#adddoctorscheduleModal2').modal({backdrop: 'static', keyboard: false});
-            Session.set('name', user.fname + " " + user.lname);
+            Session.set('name', name);
             if(time == "0") Session.set('time', 'ช่วงเช้า(9:00-12:00)');
             else Session.set('time', 'ช่วงบ่าย(13:00-16:00)');
             Session.set('date', date);
-            
           }
         });//end of meteor.call   
       },
