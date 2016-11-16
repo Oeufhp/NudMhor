e73207f155359c6188c490a249f11fea46f6ee4f
tunnuttun <tunnuttun@gmail.com>
2016-11-16 23:52:38 +0700

    implementing viewDoctorSchedule

--- client/page/doctorSchedule/viewDoctorSchedule.js
+++ client/page/doctorSchedule/viewDoctorSchedule.js
@@ -1,9 +1,29 @@
-Template.body.events({
-    'submit #viewDoctorScheduleForm': function(event){
-      event.preventDefault();
-      console.log('viewDoctorScheduleForm is submited');
-      $('#viewDoctorScheduleModal').modal('toggle');
-      Router.go('/doctorSchedule');
-    }
-});
+if(Meteor.isClient){
+  Template.body.events({
+      'submit #viewDoctorScheduleForm': function(event){
+        event.preventDefault();
+        console.log('viewDoctorScheduleForm is submited');
+        let doctorEid = event.target.doctorEid.value.trim();
+        Meteor.call('searchDoctor',doctorEid,function(err,res){
+          if(err){
+              Bert.alert({title: 'รหัสพนักงานของแพทย์ที่ระบุไม่มีอยู่ในระบบ กรุณาระบุใหม่อีกครั้ง'
+              , type: 'danger',style:'growl-top-right',icon: 'fa-key'});          
+          }
+          else{
+            Meteor.call('getDoctorSchedule',doctorEid,function(err2,schedules){
+              Session.set('schedules',schedules);
+              $('#viewDoctorScheduleModal').modal('toggle');
+              Router.go('/doctorSchedule');
+            });
+          }
+        });//end of searchDoctor call
+      },
+  });
+
+  Template.viewDoctorSchedule.helpers({
+    schedules: function(){ return Session.get('schedules'); }
+  })  
+}
+
+
 
