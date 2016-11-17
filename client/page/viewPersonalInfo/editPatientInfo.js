2b87c86a49c7ef278ae3b19e5e95ef0d1d6bd077
tunnuttun <tunnuttun@gmail.com>
2016-11-17 21:09:48 +0700

    Complete viewPatientInfo(CANNOT NOT EDIT)

--- client/page/viewPersonalInfo/editPatientInfo.js
+++ client/page/viewPersonalInfo/editPatientInfo.js
@@ -1,2 +1,28 @@
-Template.editPatientInfo.onRendered(function() {
-})
+if(Meteor.isClient){ 
+
+    Template.editPatientInfo.events({
+        // '​click #edit-patientInfo-btn':function(){
+        //     // event.preventDefault();
+        //     Router.go('/viewPatientInfo/edit');
+        // }
+        'submit #editPatientInfoForm':function(){
+            event.preventDefault();
+            
+        },
+    });
+
+    Template.editPatientInfo.helpers({
+        patientSearched:function(){
+            return Session.get('patientSearched');
+        },
+        gender0:function(){
+            let gender = Session.get('patientSearched').gender;
+            if(gender == "male") return "checked";
+        },
+        gender1:function(){
+            let gender = Session.get('patientSearched').gender;
+            if(gender == "female") return "checked";
+        },
+    });
+
+}
--- client/page/viewPersonalInfo/viewPatientInfo.js
+++ client/page/viewPersonalInfo/viewPatientInfo.js
@@ -1,6 +1,37 @@
-// Template.body.events({
-//     '​click #edit-patientInfo-btn':function(){
-//         // event.preventDefault();
-//         Router.go('/viewPatientInfo/edit');
-//     }    
-// });
+if(Meteor.isClient){ 
+
+    Template.viewPatientInfo.events({
+        // '​click #edit-patientInfo-btn':function(){
+        //     // event.preventDefault();
+        //     Router.go('/viewPatientInfo/edit');
+        // }
+        'click #viewPatientInfoGoBack': function(event){
+            console.log("submitted back to home");
+            event.preventDefault();
+            thisRole = Session.get('currentUser').role;
+            if(thisRole == 'nurse') Router.go('/home/nurse');
+            else if(thisRole == 'patient') Router.go('/home/patient');
+            else if(thisRole == 'doctor') Router.go('/home/doctor');
+            else if(thisRole == 'receptionist') Router.go('/home/receptionist');
+            else if(thisRole == 'pharmacist') Router.go('/home/pharmacist');
+            else Router.go('/home/homepage');
+        },    
+    });
+
+    Template.viewPatientInfo.helpers({
+        patientSearched:function(){
+            return Session.get('patientSearched');
+        },
+        gender0:function(){
+            let gender = Session.get('patientSearched').gender;
+            if(gender == "male") return "checked";
+            else return "disabled";
+        },
+        gender1:function(){
+            let gender = Session.get('patientSearched').gender;
+            if(gender == "female") return "checked";
+            else return "disabled";
+        },
+    });
+
+}
