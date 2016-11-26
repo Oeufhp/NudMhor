40f238e0ddba0aee928940925cf8fb20020bbcfe
Nattanai Amorntevapat <ink.firesonic@gmail.com>
2016-11-26 08:50:31 +0700

    finish view doctor info

--- client/page/manageHospitalEmployee/DoctorInfo.js
+++ client/page/manageHospitalEmployee/DoctorInfo.js
@@ -3,25 +3,31 @@ Template.body.events({
         $('#searchDocName-modal').modal('hide');
         let doc = $('#viewDocInfo_docSel option:selected').val();
         console.log('docid: ',doc);
-        // Meteor.call('viewDoctorInfo_search',doc,function(err,result){
-        //     console.log(result);
-        //     Session.set('viewDocInfo_selDoc',result);
-        // });
-        //Session.set('viewDocInfo_selDoc',);
+        Meteor.call('viewDoctorInfo_search',doc,function(err,result){
+            console.log(result);
+            Session.set('viewDocInfo_selDoc',result);
+        });
         $('#viewDocInfoModal').modal('show');
         
     },
     'change #viewDocInfo_depSel':function(event){
         console.log('click selector');
         let depart = $('#viewDocInfo_depSel option:selected').text();
-        Session.set('viewDocInfo_selDep',depart);
+        let doctors = User.find({department:depart,role:"doctor"}).fetch();
+        console.log(depart);
+        $('.docList').remove();
+        for(let i=0;i<doctors.length;i++){
+          $('#viewDocInfo_docSel').append("<option class='docList' data-eid='"+doctors[i].eid+"' value='"+doctors[i].eid+"'>"+doctors[i].fname+" "+doctors[i].lname+"</option>")
+        }
       }
 });
 
 Template.body.helpers({
-    'viewDoctorInfo_doctor_list': function(){
-      let depart = Session.get('viewDocInfo_selDep');
-      console.log('helpers',User.find({role:"doctor",department:depart}).fetch());
-      return User.find({role:"doctor",department:depart}).fetch();
+    'viewDoctorInfo': function(){
+        let doctor = Session.get('viewDocInfo_selDoc');
+        console.log('get doctor: ',doctor);
+        return doctor;
+
     }
+
 });
